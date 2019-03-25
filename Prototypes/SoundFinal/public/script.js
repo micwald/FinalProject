2bdd144a0421622e63175feceb66163ade0e653b
mounika <mounikadamerase@gmail.com>
2019-03-25 15:02:00 +0100

    done

---
+++ Prototypes/SoundFinal/public/script.js
@@ -0,0 +1,52 @@
+var lastMsgEl = null;
+if (document.readyState != 'loading') onDocumentReady();
+else document.addEventListener('DOMContentLoaded', onDocumentReady);
+
+function handleCommand(d) {
+   lastMsgEl.innerHTML =  `text: ${d.text}`;
+}
+
+function onDocumentReady() {
+    var socket = new ReconnectingWebsocket("ws://" + location.host + "/serial");
+    var sendFormEl = document.getElementById('sendForm');
+    var lastMsg = null;
+    lastMsgEl = document.getElementById('lastMsg');
+    socket.onmessage = function(evt) {
+        // Debug: see raw received message
+        //console.log(evt.data);
+        
+         // Parse message, assuming <Text,Int>
+         var d = evt.data.trim();
+         if (d.charAt(0) == '<' && d.charAt(d.length-1) == '>') {
+             // Looks legit
+             d = d.split(',');    
+             if (d.length == 1) { // Yes, it has two components as we hoped
+                 handleCommand({
+                     text:d[0].substr(1), 
+                     //integer: parseInt(d[1]), 
+                 });
+                 return;          
+             }
+         }         
+        
+        
+        // Doesn't seem to be formatted correctly, just display as-is
+        if (evt.data != lastMsg) {
+            lastMsgEl.innerText =  evt.data;
+            lastMsg = evt.data;
+        }
+    }
+    socket.onopen = function(evt) {
+        console.log("Socket opened");
+    }
+
+    sendFormEl.addEventListener('submit', function(evt) {
+        evt.preventDefault();
+        var send = document.getElementById('sendtoSerial').value;
+       // console.log(send);
+       // console.log("I am in script.js at the end");
+        socket.send(send);  
+    })
+    
+      
+}
